448f2f1aad8bc2f9cf3e677089c50a9fdc542973
iamcoder18 <iamcoder18@gmail.com>
2024-07-09 06:54:45 -0600

    Commit Before Changed To Sending Image

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import time
 import base64
+
+import PIL.Image
+from PIL import Image
 import numpy as np
-from flask import Flask, request
+from flask import Flask, request, send_file
 from flask_cors import CORS
 from sahi.utils.cv import (get_bool_mask_from_coco_segmentation, read_image_as_pil)
-from ultralyticsplus import YOLO
+from ultralyticsplus import YOLO, render_result
+import io
 
 app = Flask(__name__)
 CORS(app)
@@ -17,11 +21,67 @@ model.overrides['agnostic_nms'] = False
 model.overrides['max_det'] = 1000
 
 
-@app.route("/", methods=['POST'])
-def upload_image():
-    start_time = time.time()
+# @app.route("/", methods=['POST'])
+# def detect():
+#     start_time = time.time()
+#
+#     data = request.json
+#
+#     head, data = str(data.get('filename')).split(",", 1)
+#
+#     mime_type = head.split(";")[0].split("/")[1]
+#     filename = f"image.{mime_type}"
+#
+#     decoded_data = base64.b64decode(data)
+#
+#     with open(filename, 'wb') as f:
+#         f.write(decoded_data)
+#
+#     print(f"Data URI saved as: {filename}")
+#
+#     results = model.predict(filename)
+#
+#     image = read_image_as_pil(filename)
+#     np_image = np.ascontiguousarray(image)
+#
+#     names = model.model.names
+#
+#     masks = results[0].masks
+#     boxes = results[0].boxes
+#
+#     object_predictions = []
+#     if boxes is not None:
+#         det_ind = 0
+#         for xyxy, conf, cls in zip(boxes.xyxy, boxes.conf, boxes.cls):
+#             if masks:
+#                 img_height = np_image.shape[0]
+#                 img_width = np_image.shape[1]
+#                 segments = masks.segments
+#                 segments = segments[det_ind]
+#                 segments[:, 0] = segments[:, 0] * img_width
+#                 segments[:, 1] = segments[:, 1] * img_height
+#                 segmentation = [segments.ravel().tolist()]
+#
+#                 bool_mask = get_bool_mask_from_coco_segmentation(
+#                     segmentation, width=img_width, height=img_height
+#                 )
+#                 if sum(sum(bool_mask == 1)) <= 2:
+#                     continue
+#                 object_predictions.append([[img_height, img_width], names[int(cls)], round(conf.item() * 100)])
+#             else:
+#                 object_predictions.append([xyxy.tolist(), names[int(cls)], round(conf.item() * 100)])
+#             det_ind += 1
+#
+#     end_time = time.time()
+#     execution_time = end_time - start_time
+#     print(f"Detection time: {execution_time:.2f} seconds")
+#     return {"result": object_predictions}, 200
+#     # return "Ok", 200
 
+@app.route("/", methods=['POST'])
+def detect():
     data = request.json
+
     head, data = str(data.get('filename')).split(",", 1)
 
     mime_type = head.split(";")[0].split("/")[1]
@@ -36,41 +96,21 @@ def upload_image():
 
     results = model.predict(filename)
 
-    image = read_image_as_pil(filename)
-    np_image = np.ascontiguousarray(image)
+    print(results)
 
-    names = model.model.names
+    print(results[0].boxes)
+    # render = render_result(model=model, image=filename, result=results[0])
+    plainImage = Image.open(filename)
 
-    masks = results[0].masks
-    boxes = results[0].boxes
-
-    object_predictions = []
-    if boxes is not None:
-        det_ind = 0
-        for xyxy, conf, cls in zip(boxes.xyxy, boxes.conf, boxes.cls):
-            if masks:
-                img_height = np_image.shape[0]
-                img_width = np_image.shape[1]
-                segments = masks.segments
-                segments = segments[det_ind]
-                segments[:, 0] = segments[:, 0] * img_width
-                segments[:, 1] = segments[:, 1] * img_height
-                segmentation = [segments.ravel().tolist()]
-
-                bool_mask = get_bool_mask_from_coco_segmentation(
-                    segmentation, width=img_width, height=img_height
-                )
-                if sum(sum(bool_mask == 1)) <= 2:
-                    continue
-                object_predictions.append([[img_height, img_width], names[int(cls)], round(conf.item() * 100)])
-            else:
-                object_predictions.append([xyxy.tolist(), names[int(cls)], round(conf.item() * 100)])
-            det_ind += 1
-
-    end_time = time.time()
-    execution_time = end_time - start_time
-    print(f"Detection time: {execution_time:.2f} seconds")
-    return {"result": object_predictions}, 200
+    render = render_result(model=model, result=results[0], image=plainImage)
+    # render.save("result.png")
+    # return send_file(
+    #     "result.png", mimetype='image/png'
+    # ), 200
+    data = io.BytesIO()
+    render.save(data, "PNG")
+    data64 = base64.b64encode(data.getvalue())
+    return {"result": u'data:image/png;base64,' + data64.decode('utf-8')}, 200
 
 
 @app.route("/activate", methods=['GET'])
